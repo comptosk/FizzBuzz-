21537d0be11bcff56b190265a770624e0e933c17
max-student <max-student@maxtrain.com>
2020-06-18 11:15:42 -0400

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizzBuzz {
+    class Program {
+        static void Main(string[] args) {
+
+            for (var idx = 1; idx <= 30; idx++) {
+                if (idx % 3 == 0 && idx % 5 == 0)
+                    Console.Write($"  {idx} FIZZBUZZ ");
+                else
+                if (idx % 3 == 0)
+                    Console.Write($" {idx} FIZZ ");
+                else
+                    if (idx % 5 == 0)
+                    Console.Write($" {idx} BUZZ ");
+
+            }
+        }
+    }
+}
